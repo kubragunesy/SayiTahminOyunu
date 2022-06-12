c544e24226805dcdda7ff18a5606720ccadc16f0
kubragunesy <kubragunes76@gmail.com>
2022-06-12 16:34:04 +0300

    Random Sayı Oyunu

---
+++ SayıTahminOyunu/SayıTahminOyunu/Program.cs
@@ -0,0 +1,27 @@
+﻿Random rastgele = new Random();
+int tahmin = rastgele.Next(101);
+int sayi;
+
+while (true)
+{
+    Console.WriteLine("1-100 arasında bir sayi giriniz:");
+    sayi = Convert.ToInt32(Console.ReadLine());
+
+    if (tahmin > sayi)
+    {
+        Console.WriteLine("Daha büyük bir sayi giriniz:");
+
+    }
+
+    else if (tahmin < sayi)
+            {
+        Console.WriteLine("Daha küçük bir sayi giriniz:");
+
+    }
+     else
+    {
+        Console.WriteLine("Tebrikler");
+    }
+}
+
+Console.ReadKey();  
